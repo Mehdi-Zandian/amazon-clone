5310d236c2cc7e1373c2df99e8f3719731f2ccc7
MehdiZandian <mahdizandi81@gmail.com>
2021-12-30 16:04:32 +0330

    Update Checkout UI

--- src/components/CheckoutScreen/CheckoutScreen.js
+++ src/components/CheckoutScreen/CheckoutScreen.js
@@ -9,6 +9,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 // UI
 import Exclusive from "../../assets/exclusive/exclusive.jpg";
 import "./CheckoutScreen.scss";
+import emptyCart from "../../assets/Empty/empty-cart.svg";
+import { AiFillEuroCircle } from "react-icons/ai";
 
 function CheckoutScreen() {
   // show loader of proceed btn
@@ -23,6 +25,13 @@ function CheckoutScreen() {
       type: "CLEAR_BASKET",
     });
   };
+  const clearBasketLoader = () => {
+    setLoaderShow(true);
+    setTimeout(() => {
+      setLoaderShow(false);
+      clearBasket();
+    }, 2500);
+  };
   // add proceeded products to order list
   const addToOrderList = () => {
     setLoaderShow(true);
@@ -36,31 +45,53 @@ function CheckoutScreen() {
       });
       // clear basket after proceed to checkout
       clearBasket();
-    }, 2000);
+    }, 2500);
   };
   return (
     <div className="checkout">
       <div className="checkout__wrapper mx-auto d-flex flex-md-row flex-column">
-        <div>
-          <img
-            className="checkout__banner img-fluid w-100"
-            src={Exclusive}
-            alt="Amazon exclusive"
-          />
-
+        <div className="w-100">
           {basket?.length === 0 ? (
-            <div>
-              <h3 className="fw-bold p-2">
+            <div className="w-100">
+              <img src={emptyCart} className="col-4 mt-4 mx-auto d-block" />
+              <span className="fw-bold d-inline-block p-2 text-center mt-3 w-100">
                 Your Shopping basket is empty ... <br />{" "}
-                {order.length > 0 && (
-                  <Link to="/order" className="fs-5">
+                {order.length > 0 ? (
+                  <Link
+                    to="/order"
+                    style={{
+                      fontSize: "14px",
+                      width: "250px",
+                      backgroundColor: "#febd69",
+                      color: "#131921",
+                    }}
+                    className="py-1 d-inline-block shadow mt-2 mb-5 rounded text-decoration-none"
+                  >
                     Checkout Order List
                   </Link>
+                ) : (
+                  <Link
+                    to="/"
+                    style={{
+                      fontSize: "14px",
+                      width: "250px",
+                      backgroundColor: "#febd69",
+                      color: "#131921",
+                    }}
+                    className="py-1 d-inline-block shadow mt-2 mb-5 rounded text-decoration-none"
+                  >
+                    Back To Home
+                  </Link>
                 )}
-              </h3>
+              </span>
             </div>
           ) : (
             <div className="p-2 bg-white shadow-sm">
+              <img
+                className="checkout__banner img-fluid w-100"
+                src={Exclusive}
+                alt="Amazon exclusive"
+              />
               <h2 className="fw-bold border-bottom py-2 border-2">
                 Your shopping basket
               </h2>
@@ -78,11 +109,15 @@ function CheckoutScreen() {
                 );
               })}
               <button
-                onClick={() => clearBasket()}
+                onClick={() => clearBasketLoader()}
                 title="You will not be able to see the Basket items anymore !"
-                className="btn btn-sm btn-danger mx-auto d-block mb-5 shadow w-50"
+                className="btn btn-sm btn-danger mx-auto d-block mb-5 shadow w-50 py-2"
               >
-                Clear Basket !
+                {loaderShow ? (
+                  <span className="loader"></span>
+                ) : (
+                  "Clear Basket !"
+                )}
               </button>
             </div>
           )}
@@ -91,18 +126,29 @@ function CheckoutScreen() {
         {basket.length > 0 && (
           <div className="d-flex flex-column bg-white p-3 my-md-0 my-3 col-md-2 col-12 shadow-sm">
             <>
-              <h6 style={{ fontSize: "15px" }} className="fw-bold px-2">
-                Subtotal ({basket.length} items): {`£${getBasketTotal(basket)}`}
-              </h6>
+              <div style={{ fontSize: "15px" }} className="fw-bold">
+                <p>
+                  <span>
+                    <AiFillEuroCircle className="fs-5" /> Order Total
+                    <small className="text-danger mx-1">
+                      ({basket.length} items)
+                    </small>
+                    :
+                  </span>
+                </p>
+                <p className="w-100 p-1 text-center bg-danger text-white my-1 rounded">
+                  £{getBasketTotal(basket)}
+                </p>
+              </div>
               <button
-                disabled={!isAuthenticated}
+                disabled={!true}
                 onClick={() => addToOrderList()}
                 className={`checkout__btnStyle btn p-1 shadow-none text-dark ${
-                  !isAuthenticated && "checkout__btnSignin text-white"
+                  !true && "checkout__btnSignin text-white"
                 }`}
                 style={{ fontSize: "15px" }}
               >
-                {isAuthenticated ? (
+                {true ? (
                   loaderShow ? (
                     <span className="loader"></span>
                   ) : (
